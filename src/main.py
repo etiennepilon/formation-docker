426e8028c3d98ca803497e7aabc635b20a018cd3
Etienne Pilon <etienne.b.pilon@gmail.com>
2020-01-27 17:17:23 -0500

    Adds environment variables for Host

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import os
+
+HOSTNAME = os.getenv("HOSTNAME", default="127.0.0.1")
 
 app = Flask(__name__)
 
@@ -11,5 +14,4 @@ def about():
     return render_template("about.html")
     
 if __name__ == "__main__":
-    app.run(port=5000)
-    #app.run(host='0.0.0.0', port=5000)
+    app.run(host=HOSTNAME, port=5000)
